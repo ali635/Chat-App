6d1c15164af104536621440045cb5225218cb2f5
Mohamed <mohamedali7659@gmail.com>
2021-03-30 18:18:10 +0200

    Make Group chat app using Laravel Echo

---
+++ resources/js/bootstrap.js
@@ -0,0 +1,15 @@
+import 'alpinejs';
+
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allows your team to easily build robust real-time web applications.
+ */
+
+ import Echo from 'laravel-echo';
+ import Larasocket from 'larasocket-js';
+ 
+ window.Echo = new Echo({
+     broadcaster: Larasocket,
+     token: process.env.MIX_LARASOCKET_TOKEN,
+ });
